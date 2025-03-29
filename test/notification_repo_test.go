d56896d886f3f3182bcfbe71dbc497120ad2680d
bvnnal <bamapinya2503@gmail.com>
2025-03-29 19:13:07 +0700

    CI/CD

--- test/notification_repo_test.go
+++ test/notification_repo_test.go
@@ -1,3 +1,152 @@
+bvnn
+bvnnal
+ไม่ระบุ
+
+H O M I E — 25/3/68 18:31
+ได้
+H O M I Eปักหมุดข้อความในช่องนี้แล้ว ดูข้อความที่ปักหมุดทั้งหมด — 25/3/68 18:32
+FLUKKY — 25/3/68 18:32
+เขียน cucumber test ไม่เป็น
+PinkPPanther — 25/3/68 18:32
+ไม่เป็นเหมือนกัน
+แต่จะพยายามไปรีเสิชมาให้
+H O M I E — 25/3/68 18:32
+เดี๋ยวไปดูมาใฟ้
+คืนนี้แหละ
+ไม่ต้องกลัว
+FLUKKY — 25/3/68 18:32
+แต่มีในที่เรียนชะ
+PinkPPanther — 25/3/68 18:32
+มี
+H O M I E — 25/3/68 18:33
+ลองไปดู cucumber.io น่าจะมี doc นะ มีหลายภาษาเลย
+PinkPPanther — 25/3/68 18:33
+ภาพ
+อันนี้ java
+H O M I E — 25/3/68 18:33
+ข้าวซอยเอาเส้นปะ
+ด่วน
+PinkPPanther — 25/3/68 18:33
+ไม่
+เอาแค่เกาเหลา
+PinkPPanther — 25/3/68 18:34
+แต่ cucumber ใช้กับ next.js ได้เหมือนกัน 
+PinkPPanther — 25/3/68 18:34
+@mmerized @zertainly @bvnn
+H O M I E — 25/3/68 18:39
+ภาพ
+เลือกทำนะ
+PinkPPanther — 25/3/68 18:44
+ของเราอันบนเค้าให้เลือกทำอย่างใดอย่างนึง 
+FLUKKY — 25/3/68 18:44
+ใน click up ไม่มีอะไรเลย ??
+ภาพ
+H O M I E — 25/3/68 18:44
+กด board
+มัน complete แล้วมันเลยไม่โชใน todo
+@everyone เราดลือกให้เลยนะ เราจะไปทำ test ที่หลังบ้านกันเทสเป็น module
+โดยจะเลือกเทสตาม feature โดยเลือกใหม่เลย ไม่เอาตามใน click up เพราะว่ามันต้องเลือกเทสคนละฟีเจอร์
+ก็คือในทุกๆ 1 ฟีเจอรจะมี 3 user story และในแต่ละ user storu ก็จะมี acceptance criteria
+mmerized — 25/3/68 19:28
+😅
+H O M I E — 25/3/68 19:38
+❤️ [Feature] Customer Loyalty Management
+✅ [Feature] Leftover Food Fee Calculation
+👉 [Feature] Best-Selling Menu Insights
+🕕 [Feature] Call Staff Assistance
+@everyone มากดเลือกฟีเจอร์ที่ตัวเองจะทำ test
+mmerized — 25/3/68 19:50
+สอบถามหน่อยย คือ 28,29 นี้ มี webtech, pm แล้วที่เหลือของโปรเจค agile อะ ทำใน 30,31 จะทันมั้ย
+PinkPPanther — 25/3/68 19:51
+Deadlineคือ 30 อ่ะ
+ถ้านี่คิดคิดว่า 1-2 วันเสร็จ พวก testing 
+mmerized — 25/3/68 20:03
+แล้วส่วนที่ไม่ใช่ testing นี่ถึงไหนแล้วอะ
+PinkPPanther — 25/3/68 20:04
+หมายถึงส่วนโค้ดหรือส่วนไหน
+mmerized — 25/3/68 20:04
+ทั้งหมดเลย
+PinkPPanther — 25/3/68 20:05
+. ตามนี่
+ส่วนใหญ่เป็นพวก doc readme ไรงี้มากกว่า ที่เหลือน่าจะไม่ละ + ตามหา bug + CICD 
+H O M I E — เมื่อวานนี้ เวลา 15:01
+@everyone ผมส่ง feature test ของทุกคนให้ไปในแชทส่วนตัวแล้วนะ
+ให้ทุกคนนำ สร้าง feature ตามชื่อที่พิมพ์ไปให้ แล้ววางไฟล์ไว้ที่ folder test จากนั้น git add . แล้วก็ git commit แล้วก็ git flow feature publish [feature name]
+แล้วก็ finish feature หลังจากนั้นก็ push develop
+ขอบคุณคับ
+H O M I E — เมื่อวานนี้ เวลา 15:17
+ลืมบอกนะ ว่าใน backend
+H O M I E — 13:45
+ภาพ
+H O M I E — 14:04
+@everyone วันนี้มาทำงานลกุ่มกันเพื่อนว่างกันกี่โมง
+5️⃣ โมง
+6️⃣ โมง
+1️⃣ ทุ่ม
+2️⃣ ทุ่ม
+🙃 ไม่ว่างเลย 
+PinkPPanther — 14:06
+ภาพ
+H O M I E — 14:42
+ภาพ
+PinkPPanther — 15:15
+Create a screen to display member when use redeems/adds points.
+PinkPPanther — 15:27
+push readme แล้วเช็คด้วย ชื่อ นามสกลุ รหัสนิสิต feature ของตัวเอง
+H O M I E — 16:08
+@everyone 17.00 ขอนัดทุกคนมาเคลียร์งานกลุ่มให้จบวันนี้น้า
+FLUKKY — 16:50
+ขอเลท 10 นาทีนะ
+bvnn — 16:55
+งั้นเดี๋ยวแบมเข้าพร้อมฟลุ๊คเลยๆๆ
+H O M I E — 16:57
+17.30 ไหม
+bvnn — 17:02
+ด้ายยย
+H O M I E — 17:37
+https://docs.google.com/spreadsheets/d/1ZdJDxWnTgpx-1vnaj0YBoItpxXMgEJZddDhhK9xGj8o/edit?usp=sharing
+Google Docs
+472-Final Project-Checklist-
+ภาพ
+H O M I E — 17:58
+https://ladyusa.notion.site/S1-CI-CD-5-180f199d3b158129bb73df1578e128d2
+ladyusa on Notion
+S1 - CI/CD (5%) | Notion
+Pre-Requisite
+S1 - CI/CD (5%) | Notion
+homiez09
+PinkPPanther — 18:03
+ภาพ
+H O M I E — 18:03
+bvnnal, homiez09, malaming, aquarat101, flukkies 
+https://github.com/orgs/472-G-Buffet-POS-AAA/repositories
+GitHub
+472-G-Buffet POS-AAA
+472-G-Buffet POS-AAA has 2 repositories available. Follow their code on GitHub.
+472-G-Buffet POS-AAA
+bvnn — 18:15
+ภาพ
+H O M I E — 18:16
+ภาพ
+prod.Dockerfile
+H O M I E — 18:24
+FROM golang:1.23.3-alpine AS builder
+WORKDIR /app
+COPY . .
+RUN go mod tidy
+RUN go test ./test
+RUN export $(cat .env | xargs)
+CMD ["go", "run", "main.go"]
+H O M I E — 18:37
+package test
+
+import (
+"context"
+"testing"
+ขยาย
+notification_repo_test.go
+3 KB
+﻿
 package test
 
 import (
@@ -43,7 +192,7 @@ func (m *MockNotificationRepository) Delete(ctx context.Context) error {
 func TestCreateNotification(t *testing.T) {
 	mockRepo := new(MockNotificationRepository)
 	req := &requests.StaffNotificationRequest{
-		TableID: uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
+		TableID: uuid.New(),
 	}
 
 	mockRepo.On("Create", context.Background(), req).Return(nil)
@@ -58,8 +207,8 @@ func TestShowListOfNotification(t *testing.T) {
 	mockRepo := new(MockNotificationRepository)
 
 	mockNotification := &models.StaffNotification{
-		ID:      uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
-		TableID: uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
+		ID:      uuid.New(),
+		TableID: uuid.New(),
 	}
 
 	mockRepo.On("FindAll", context.Background()).Return([]*models.StaffNotification{mockNotification}, nil)
@@ -74,7 +223,7 @@ func TestShowListOfNotification(t *testing.T) {
 func TestUpdateNotification(t *testing.T) {
 	mockRepo := new(MockNotificationRepository)
 	req := &requests.StaffNotificationRequest{
-		TableID: uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
+		TableID: uuid.New(),
 	}
 
 	mockRepo.On("Update", context.Background(), req).Return(nil)
@@ -100,8 +249,8 @@ func TestShowNotificationByID(t *testing.T) {
 	mockRepo := new(MockNotificationRepository)
 
 	mockNotification := &models.StaffNotification{
-		ID:      uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
-		TableID: uuid.MustParse("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
+		ID:      uuid.New(),
+		TableID: uuid.New(),
 	}
 
 	mockRepo.On("FindByID", context.Background()).Return(mockNotification, nil)
